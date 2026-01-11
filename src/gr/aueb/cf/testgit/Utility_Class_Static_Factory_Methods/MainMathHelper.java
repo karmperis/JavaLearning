cd0b4ec97f9ca5da3c80e86f4c10397df30b1c4e
nikolaos.karberis <nikolaos.karberis@gmail.com>
2026-01-11 18:28:35 +0200

    feat:exercise: utility class

---
+++ src/gr/aueb/cf/testgit/Utility_Class_Static_Factory_Methods/MainMathHelper.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.cf9.exercises.Utility_Class_Static_Factory_Methods;
+
+/**
+ * Uses utility class math helper.
+ */
+public class MainMathHelper {
+    public static void main(String[] args) {
+        int a = 19;
+        int b = 100;
+
+        System.out.println("Max number: " + MathHelper.max(a, b));
+    }
+}
---
+++ src/gr/aueb/cf/testgit/Utility_Class_Static_Factory_Methods/MathHelper.java
@@ -0,0 +1,15 @@
+package gr.aueb.cf.cf9.exercises.Utility_Class_Static_Factory_Methods;
+
+/**
+ * Exercise: Utility class math helper.
+ */
+public class MathHelper {
+
+    private MathHelper(){
+
+    }
+
+    public static int max(int a, int b){
+        return (a > b) ? a : b;
+    }
+}
