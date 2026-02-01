a67444ebe308564ccd9ea185c54dcdd2fe7d33c0
Nikolaos Karmperis <karmperis@gmail.com>
2026-02-01 18:28:04 +0200

    fix: dependency injection wiring in Main

--- src/gr/aueb/cf/java/Interfaces/MainSpeakable.java
+++ src/gr/aueb/cf/java/Interfaces/MainSpeakable.java
@@ -7,11 +7,13 @@ public class MainSpeakable {
     public static void main(String[] args) {
         ISpeakable alice = new Cat("Alice");
         ISpeakable bob = new Dog("Bob");
-        ISpeakable whiteCat = new Cat("White cat");
+        GenericSpeakingSchool catSchool = new GenericSpeakingSchool(alice);
+        GenericSpeakingSchool dogSchool = new GenericSpeakingSchool(bob);
 
         doSpeak(alice);
         doSpeak(bob);
-        doSpeak(whiteCat);
+        catSchool.learnToSpeak();
+        dogSchool.learnToSpeak();
 
     }
 
